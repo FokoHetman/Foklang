7682d145e4b4eb26c66c0c8fdf970eb8105c5820
Hetman Foko <72759439+FokoHetman@users.noreply.github.com>
2024-06-07 08:48:10 +0200

    Update shell.rs

--- shell.rs
+++ shell.rs
@@ -8,7 +8,9 @@ fn main() {
   let shell: &str = "foklang";
   let mut input = String::new();
   let tokenizer = core::tokenizer::Tokenizer {};
-  let parser = core::parser::Parser {};
+  let mut parser = core::parser::Parser {};
+  let error_handler = core::error_handler::ErrorHandler {};
+  let interpreter = core::interpreter::Interpreter {error_handler: error_handler};
   loop {
     print!("{}$ ", shell);
     input = String::new();
@@ -20,5 +22,8 @@ fn main() {
     let mut parsed_input = parser.parse(tokenized_input);
 
     println!("Parser Out: {:#?}", parsed_input);
+
+    let mut interpreted_input = interpreter.evaluate(parsed_input);
+    println!("Interpreter Out: {:#?}", interpreted_input);
   }
 }
