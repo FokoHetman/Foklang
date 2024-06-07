69a4429913e98a74d04fa73d2f9794a6d27eaaa8
Hetman Foko <72759439+FokoHetman@users.noreply.github.com>
2024-06-07 08:51:50 +0200

    Update core

---
+++ core/error_handler.rs
@@ -0,0 +1,68 @@
+use core::tokenizer::{Operator};
+//MAKE IT USE ENUM ()THING
+#[derive(Clone, Debug)]
+pub enum NodeKind {                                                                               Program {body: Vec<Box<Node>>, id: i32},
+  Identifier {symbol: String},                                                                    NumericLiteral{ value: NodeValue},
+  Expression,                                                                                     BinaryExpression{ left: Box<Node>, right: Box<Node>, operator: Operator},
+  Stmt,
+  NullLiteral{value: NodeValue},
+  Function,
+}
+#[derive(Debug, Clone)]
+pub enum NodeValue {
+  Integer(i32),
+  String(String),
+  Nullus,
+}
+                                                                                                #[derive(Clone, Debug)]
+pub struct Node {
+  pub kind: NodeKind,
+}
+#[derive(Clone,Debug)]
+pub enum Fructa {
+  Nullus,
+  Numerum(i32),
+  Filum(String),
+}
+#[derive(Clone,Debug)]
+pub struct Proventus {
+  pub value: Fructa,
+  pub id: i32,
+}
+
+impl Default for Proventus {
+  fn default() -> Proventus {
+    Proventus{value: Fructa::Nullus, id: 0}
+  }
+}
+/*impl Default for NodeKind {                                                                     fn default() -> Self{ NodeKind::BinaryExpression{left: Box<Node>, right: Box<Node>, operator: Operator} } {//NodeKind {
+    NodeKind::BinaryExpression{left: Box::<Node>::new(Node{kind: NodeKind::NullLiteral{value: NodeValue::Nullus}}),
+        right: Box::<Node>::new(Node{kind: NodeKind::NullLiteral{value: NodeValue::Nullus}}),
+        operator: Operator::Addition,
+    }
+  }
+}
+impl Default for Node {
+  fn default() -> Node {
+    Node {
+      kind: NodeKind::NullLiteral,
+      body: None,
+      left: None,
+      right: None,
+      symbol: None,
+      operator: None,
+      value: None,
+    }
+  }
+}*/
+impl Node {
+  pub fn appendToBody(&mut self, node: Node) {
+    let mut boxed = Box::<Node>::new(node);
+    match self.kind {
+      NodeKind::Program{body: ref mut s ,id:_} => {
+        s.push(boxed);
+      }
+      _ => panic!("AST Error: Tried to append a Node to non-Program Node")
+    }
+  }
+}
