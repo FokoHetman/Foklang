5024d90fffb53a6de157083e7d1ff9e622f7eb37
Hetman Foko <72759439+FokoHetman@users.noreply.github.com>
2024-06-07 08:53:06 +0200

    Update AST.rs

--- core/parser/AST.rs
+++ core/parser/AST.rs
@@ -1,33 +1,21 @@
-#[derive(Clone, Debug)]
-pub enum NodeKind {
-  Program,
-  Identifier,
-  NumericLiteral,
-  Expression,
-  BinaryExpression,
-  Stmt,
-  NullLiteral,
-  VariableDeclaration
+use core::tokenizer::{Operator,TokenType};
+use core::AST::{Node,NodeValue};
+
+#[derive(Clone,Copy)]
+pub struct ErrorHandler {}
+
+pub struct Peritia {
+  pub bool: bool,
+  pub error_msg: String,
 }
 
-#[derive(Clone, Debug)]
-pub struct Node {
-  pub kind: NodeKind,
-  pub body: Option<Vec<Box<Node>>>,
-  pub left: Option<Box<Node>>,
-  pub right: Option<Box<Node>>,
-  pub symbol: Option<String>,
-}
+impl ErrorHandler {
 
+  pub fn check_binary_expression(self, left: i32, right: i32) -> Peritia {
+    Peritia{bool: false, error_msg: String::new()}
 
-impl Default for Node {
-  fn default() -> Node {
-    Node {
-      kind: NodeKind::NullLiteral,
-      body: None,
-      left: None,
-      right: None,
-      symbol: None,
-    }
+  }
+  pub fn check_binary_expression_division(self, left: i32, right: i32) -> Peritia {
+    Peritia{bool: right==0, error_msg: String::from("Can't divide by 0!")}
   }
 }
