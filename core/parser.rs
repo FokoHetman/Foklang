b87104b395b21a3266347799da4a74922f675450
Hetman Foko <72759439+FokoHetman@users.noreply.github.com>
2024-06-07 08:49:40 +0200

    Update parser.rs

--- core/parser.rs
+++ core/parser.rs
@@ -1,17 +1,135 @@
-mod AST;
-use core::tokenizer::{Token, TokenValue, TokenType};
+use core::AST;
+use core::tokenizer::{Token, TokenValue, TokenType, Operator};
 #[derive(Clone, Copy)]
 pub struct Parser {}
 
 impl Parser {
+  pub fn at(self, tokens: &mut Vec<Token>) -> Token {
+    return tokens[0].clone()
+  }
+  pub fn eat(&mut self, tokens: &mut Vec<Token>) -> Token {
+    let token = tokens[0].clone();
+    tokens.remove(0);
+    return token
+  }
+  pub fn eatExpect(&mut self, expectedTokenType: TokenType, err_msg: String, tokens: &mut Vec<Token>) -> Token {
+    let token = tokens[0].clone();
+    tokens.remove(0);
 
-  pub fn parse(self, static_tokens: Vec<Token>) -> AST::Node {
-    let mut result = AST::Node{kind: AST::NodeKind::Program, body: Some([].to_vec()), ..Default::default()};
+    if token.tokentype!=expectedTokenType {
+      panic!("{}", err_msg);
+    }
+    return token
+
+  }
+  pub fn parse(&mut self, static_tokens: Vec<Token>) -> AST::Node {
     let mut tokens = static_tokens.clone();
+    let mut result = AST::Node{kind: AST::NodeKind::Program{body: Vec::<Box<AST::Node>>::new(), id: 0}};
     while tokens[0].tokentype!=TokenType::EOF {
-      
+      result.appendToBody(self.parse_stmt( &mut tokens));
     }
 
     return result
+  }
+
+  pub fn parse_stmt(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    match self.at(tokens).tokentype {
+      //TokenType::Let => self.parse_var_declaration()
+      _ => self.parse_expr(tokens)
+    }
+  }
+  pub fn parse_expr(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    self.parse_additive_expr(tokens)
+
+  }
+  pub fn parse_additive_expr(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    let mut left = self.parse_multiplicative_expr(tokens);
+    while self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Addition) ||
+          self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Substraction) {
+
+      left = AST::Node{kind: AST::NodeKind::BinaryExpression {
+        left: Box::new(left),
+        operator: match self.eat(tokens).tokenvalue {
+            TokenValue::Operator(Operator::Addition) => Operator::Addition,
+            _ => Operator::Substraction
+        },
+        right: Box::<AST::Node>::new(self.parse_multiplicative_expr(tokens)),
+      }};
+    }
+    return left
+    //AST::Node{kind: AST::NodeKind::NullLiteral, ..Default::default()}//while self.at(tokens).tokentype
+  }
+  pub fn parse_multiplicative_expr(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    let mut left = self.parse_exponential_expr(tokens);
+    while self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Multiplication) ||
+          self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Division) {
+      left = AST::Node{kind: AST::NodeKind::BinaryExpression{
+        left: Box::new(left),
+        operator: match self.eat(tokens).tokenvalue {
+            TokenValue::Operator(Operator::Multiplication) => Operator::Multiplication,
+            _ => Operator::Division,
+        },
+        right: Box::<AST::Node>::new(self.parse_exponential_expr(tokens)),
+      }};
+    }
+
+    return left
+  }
+  pub fn parse_exponential_expr(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    let mut left = self.parse_primary_expr(tokens);
+    while self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Exponentiation) /*||
+          self.at(tokens).tokenvalue==TokenValue::Operator(Operator::Pierwiastekidk)*/ {
+      left = AST::Node{kind: AST::NodeKind::BinaryExpression{
+        left: Box::new(left),
+        operator: match self.eat(tokens).tokenvalue {
+            TokenValue::Operator(Operator::Exponentiation) => Operator::Exponentiation,
+            _ => Operator::Exponentiation,
+        },
+
+        right: Box::<AST::Node>::new(self.parse_primary_expr(tokens)),
+      }};
+    }
+    return left
+
+  }
+  pub fn parse_function_declaration(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    let mut left = self.parse_primary_expr(tokens);
+    if self.at(tokens).tokentype==TokenType::Let {
+      let function_id = self.eat(tokens);
+      let mut args =
+      while self.eat(tokens).tokentype==TokenType::Identifier {
+        args.push(functin_args);
+      }
+    }
+  pub fn parse_primary_expr(&mut self, tokens: &mut Vec<Token>) -> AST::Node {
+    let token = self.at(tokens).tokentype;
+    let eat = self.eat(tokens);
+    let empty_node = Box::<AST::Node>::new(AST::Node{kind: AST::NodeKind::NullLiteral{value: AST::NodeValue::Nullus}});
+    match token {
+      TokenType::Identifier => AST::Node {kind: AST::NodeKind::Identifier{symbol: eat.tokenvalue.to_string()}},
+
+      TokenType::Nullus => AST::Node {kind: AST::NodeKind::NullLiteral{value: AST::NodeValue::Nullus}},
+
+      TokenType::Operator => AST::Node {kind: AST::NodeKind::BinaryExpression{ operator:
+          match eat.tokenvalue {TokenValue::Operator(op) => op, _ => panic!("Operator is not an operator!")}, left: empty_node.clone(), right: empty_node}},
+
+      TokenType::Integer => AST::Node {kind: AST::NodeKind::NumericLiteral{ value: AST::NodeValue::Integer(
+          match eat.tokenvalue { TokenValue::Int(integer) => integer, _ => panic!("???") })}},
+
+      TokarenType::OpenParen => {
+          let value = self.parse_expr(tokens);
+          self.eatExpect(TokenType::CloseParen, "Invalid Token found, expected CloseParen `)`".to_string(), tokens);
+          return value;
+      },
+
+      TokenType::OpenCParen => {
+          //make it own type tbh
+          let value = self.parse_expr(tokens);
+          self.eatExpect(TokenType::CloseCParen, "Invalid Token found, expected CloseCParen `}`".to_string(), tokens);
+          println!("{:#?}", value);
+          return value
+      },
+      _ => panic!("Invalid Token Found: {:#?}", eat)
+    }
   }
 }
