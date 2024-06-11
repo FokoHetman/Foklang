0528fe6c3479b7c7a42570cfc4e0c7ed59f6dd9d
PoolloverNathan <nathan.kulzer@protonmail.com>
2024-06-11 02:15:49 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+# vim:ts=2:sts=2:sw=2:et:ft=nix:
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  outputs =
+    { self, nixpkgs }:
+    let
+      withPkgs = f: nixpkgs.lib.mapAttrs (k: v: f v) nixpkgs.legacyPackages;
+    in
+    {
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+      packages = withPkgs (pkgs: {
+        default = derivation {
+          name = "foklang";
+          inherit (pkgs) system;
+          builder = pkgs.rustc + /bin/rustc;
+          args = [
+            ./shell.rs
+            "-o"
+            (builtins.placeholder "out")
+          ];
+        };
+      });
+    };
+}
